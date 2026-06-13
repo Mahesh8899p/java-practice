3e02aee0fae952100437cf81e6fb3bd6fcb036b3
Aizen sama <156284808+Mahesh8899p@users.noreply.github.com>
2026-06-13 23:28:49 +0530

    changes made

--- polymorphism/main.java
+++ polymorphism/main.java
@@ -2,6 +2,7 @@ package polymorphism;
 
 public class main {
     public static void main(String[] args) {
+        // this is an example of the polymorphism
         shapes sh = new shapes();
         rectangle re = new rectangle();
         square sq = new square();
